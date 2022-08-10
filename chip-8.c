b0b065cd50800cb74565ee2e4d7b31939e7a9e9d
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2022-08-10 18:10:20 -0500

    fix guide url

--- chip-8.c
+++ chip-8.c
@@ -1,4 +1,4 @@
-// Main resource for CHIP-8 architecture and instructions: https://tobiasvl.github.io/blog/write-a-chip-8-emulator/#8xy6-and-8xye-shift
+// Main resource for CHIP-8 architecture and instructions: https://tobiasvl.github.io/blog/write-a-chip-8-emulator/
 
 #include <stdio.h>
 #include <stdlib.h>
